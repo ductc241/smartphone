4f891a8385acb5d8a5993f13ec0759d32d06550b
ductc241 <tacongduc123@gmail.com>
2022-06-19 17:52:10 +0700

    Update Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'account', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
+
   { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }
 ];
 
